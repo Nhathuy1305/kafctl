948e3b42bdce8bd91d1b22bdf23037106e6e459e
Nhathuy1305 <ITITIU20043@student.hcmiu.edu.vn>
2024-11-02 18:03:17 +0700

    testing for type formater

--- internal/acl/type-formatter_test.go
+++ internal/acl/type-formatter_test.go
@@ -1,1 +1,30 @@
 package acl
+
+import (
+	"fmt"
+	"github.com/IBM/sarama"
+	"testing"
+)
+
+func TestOperationFromString(t *testing.T) {
+	tests := []sarama.AclOperation{
+		sarama.AclOperationUnknown,
+		sarama.AclOperationAny,
+		sarama.AclOperationAll,
+		sarama.AclOperationRead,
+		sarama.AclOperationWrite,
+		sarama.AclOperationCreate,
+		sarama.AclOperationDelete,
+		sarama.AclOperationAlter,
+		sarama.AclOperationDescribe,
+		sarama.AclOperationClusterAction,
+		sarama.AclOperationDescribeConfigs,
+		sarama.AclOperationAlterConfigs,
+		sarama.AclOperationIdempotentWrite,
+	}
+	for _, tt := range tests {
+		t.Run(fmt.Sprintf("test operation %d", tt), func(t *testing.T) {
+			//stringOperation := operationT
+		})
+	}
+}
