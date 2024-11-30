6b70d15125841b37e8b797859e29b4086b8f0a41
Nhathuy1305 <ITITIU20043@student.hcmiu.edu.vn>
2024-11-30 16:04:43 +0700

    add testing for type formatter

--- internal/acl/type-formatter_test.go
+++ internal/acl/type-formatter_test.go
@@ -24,7 +24,64 @@ func TestOperationFromString(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(fmt.Sprintf("test operation %d", tt), func(t *testing.T) {
-			//stringOperation := operationT
+			stringOperation := operationToString(tt)
+			if got := operationFromString(stringOperation); got != tt {
+				t.Errorf("operationFromString() = %v, want %v", got, tt)
+			}
+		})
+	}
+}
+
+func TestPatternTypeFromString(t *testing.T) {
+	tests := []sarama.AclResourcePatternType{
+		sarama.AclPatternUnknown,
+		sarama.AclPatternAny,
+		sarama.AclPatternMatch,
+		sarama.AclPatternLiteral,
+		sarama.AclPatternPrefixed,
+	}
+	for _, tt := range tests {
+		t.Run(fmt.Sprintf("test operation %d", tt), func(t *testing.T) {
+			stringOperation := patternTypeToString(tt)
+			if got := patternTypeFromString(stringOperation); got != tt {
+				t.Errorf("patternTypeFromString() = %v, want %v", got, tt)
+			}
+		})
+	}
+}
+
+func TestPermissionTypeFromString(t *testing.T) {
+	tests := []sarama.AclPermissionType{
+		sarama.AclPermissionUnknown,
+		sarama.AclPermissionAny,
+		sarama.AclPermissionDeny,
+		sarama.AclPermissionAllow,
+	}
+	for _, tt := range tests {
+		t.Run(fmt.Sprintf("test operation %d", tt), func(t *testing.T) {
+			stringOperation := permissionTypeToString(tt)
+			if got := permissionTypeFromString(stringOperation); got != tt {
+				t.Errorf("permissionTypeFromString() = %v, want %v", got, tt)
+			}
+		})
+	}
+}
+
+func TestResourceTypeFromString(t *testing.T) {
+	tests := []sarama.AclResourceType{
+		sarama.AclResourceUnknown,
+		sarama.AclResourceAny,
+		sarama.AclResourceTopic,
+		sarama.AclResourceGroup,
+		sarama.AclResourceCluster,
+		sarama.AclResourceTransactionalID,
+	}
+	for _, tt := range tests {
+		t.Run(fmt.Sprintf("test operation %d", tt), func(t *testing.T) {
+			stringOperation := resourceTypeToString(tt)
+			if got := resourceTypeFromString(stringOperation); got != tt {
+				t.Errorf("resourceTypeFromString() = %v, want %v", got, tt)
+			}
 		})
 	}
 }
